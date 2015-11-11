4590c01cdb5426443cbaef9298857eb988ce2fe2
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2015-11-11 12:12:40 -0800

    Plot intensities and energy for segments

--- src/main/java/rejfree/models/normal/PlotExample.java
+++ src/main/java/rejfree/models/normal/PlotExample.java
@@ -1,18 +1,24 @@
 package rejfree.models.normal;
 
+import java.io.File;
 import java.util.List;
 
 import org.apache.commons.math3.analysis.MultivariateFunction;
 import org.jblas.DoubleMatrix;
 
 import rejfree.PlotTrajectory;
+import rejfree.StaticUtils;
+import rejfree.local.CollisionFactor;
 import rejfree.local.LocalRFRunner;
 import rejfree.local.LocalRFRunnerOptions;
 import rejfree.mixture.MixtureMain.LocAtCollisionsProcessor;
+import bayonet.opt.DifferentiableFunction;
 import bayonet.rplot.PlotContour;
+import bayonet.rplot.RUtils;
 import blang.ProbabilityModel;
 import blang.annotations.DefineFactor;
 import blang.variables.RealVariable;
+import briefj.OutputManager;
 import briefj.opt.Option;
 import briefj.opt.OptionSet;
 import briefj.run.Mains;
@@ -101,5 +107,77 @@ public class PlotExample implements Runnable
     plot.yBounds = new double[]{-bound,bound};
     plot.toPDF(Results.getFileInResultFolder("traj.pdf"));
     
+    for (int i = 0; i < lcp.locationsAtCollisions.size() - 1; i++)
+    {
+      DoubleMatrix 
+        start = lcp.locationsAtCollisions.get(i),
+        end = lcp.locationsAtCollisions.get(i+1);
+      File dir = Results.getFolderInResultFolder("seg" + i);
+      plotSegmentCurves(start, end, energyFunction(modelSpec.factor), dir, 1000);
+    }
+  }
+  
+  public static DifferentiableFunction energyFunction(CollisionFactor factor)
+  {
+    return new DifferentiableFunction() {
+      
+      private void setTo(double [] x)
+      {
+        for (int i = 0; i < factor.nVariables(); i++)
+          factor.getVariable(i).setValue(x[i]);
+      }
+      
+      @Override
+      public double valueAt(double[] x)
+      {
+        setTo(x);
+        return -factor.logDensity();
+      }
+      
+      @Override
+      public int dimension()
+      {
+        return factor.nVariables();
+      }
+      
+      @Override
+      public double[] derivativeAt(double[] x)
+      {
+        setTo(x);
+        DoubleMatrix gradient = factor.gradient();
+        gradient.muli(-1);
+        return gradient.data;
+      }
+    };
+  }
+  
+  public static void plotSegmentCurves(DoubleMatrix start, DoubleMatrix end, DifferentiableFunction energyFunction, File dir, int nPoints)
+  {
+    OutputManager out = new OutputManager();
+    out.setOutputFolder(dir);
+    
+    // plot the energy
+    DoubleMatrix diff = end.sub(start);
+    double time = diff.norm2();
+    DoubleMatrix direction = diff.div(time);
+    
+    double deltaT = time/nPoints;
+    for (int i = 0; i < nPoints; i++)
+    {
+      double curT = deltaT*i;
+      DoubleMatrix position = StaticUtils.position(start, direction, curT);
+      
+      double energy = energyFunction.valueAt(position.data);
+      
+      double dotProd = direction.dot(new DoubleMatrix(energyFunction.derivativeAt(position.data)));
+      
+      out.write("energy", "t", curT, "name", "energy",     "value", energy);
+      out.write("deriv", "t", curT, "name", "derivative", "value", dotProd);
+      out.write("deriv", "t", curT, "name", "intensity",  "value", Math.max(0,dotProd));
+    }
+    out.close();
+    
+    RUtils.callGeneratedRScript("/rejfree/plotEnergyAndIntensity.txt", dir);
+
   }
 }
